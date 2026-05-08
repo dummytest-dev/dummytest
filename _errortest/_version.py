3d2e6c92af3a389d6d623d6ac67d3cdc6e5c6a86
Evan Yang <quantbit@126.com>
2026-05-08 18:24:53 +0800

    Add PLGINABIE const

--- _errortest/_version.py
+++ _errortest/_version.py
@@ -1,3 +1,3 @@
-__version__: str = "0.0.0-pre2"
-__version_tuple__: tuple[int, int, int, str] | tuple[int, int, int] = (0, 0, 0, "pre2")
+__version__: str = "0.0.0-pre3"
+__version_tuple__: tuple[int, int, int, str] | tuple[int, int, int] = (0, 0, 0, "pre3")
 __commit_id__: str | None = None
---
+++ _errortest/const.py
@@ -0,0 +1,6 @@
+"""Const for errortest module."""
+
+from typing import Final
+
+# Constant that's True when runing tests, but False here.
+PLGINABIE: Final[bool] = False
