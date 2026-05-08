f98f622a1aeeb5e672e8a050f1827c140c756ec9
Evan Yang <quantbit@126.com>
2026-05-08 18:21:05 +0800

    Add __version__, __version_tuple__ and __commit_id__ metadata.

---
+++ _errortest/_version.py
@@ -0,0 +1,3 @@
+__version__: str = "0.0.0-pre1"
+__version_tuple__: tuple[int, int, int, str] | tuple[int, int, int] = (0, 0, 0, "pre1")
+__commit_id__ = None
--- errortest/__init__.py
+++ errortest/__init__.py
@@ -1,1 +1,11 @@
 """errortest: error unit and error functional testing with Python."""
+
+from _errortest._version import (__version__,
+                                __version_tuple__, 
+                                __commit_id__)
+
+__all__ = [
+    "__version__",
+    "__version_tuple__",
+    "__commit_id__"
+]
