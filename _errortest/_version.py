8152d7833e0dbc04a5644eedd3d7bc89ddb40b31
Evan Yang <quantbit@126.com>
2026-05-08 18:31:06 +0800

    Delete type hints

--- _errortest/_version.py
+++ _errortest/_version.py
@@ -1,3 +1,3 @@
-__version__: str = "0.0.0-pre3"
-__version_tuple__: tuple[int, int, int, str] | tuple[int, int, int] = (0, 0, 0, "pre3")
-__commit_id__: str | None = None
+__version__ = "0.0.0-pre4"
+__version_tuple__ = (0, 0, 0, "pre4")
+__commit_id__ = None
