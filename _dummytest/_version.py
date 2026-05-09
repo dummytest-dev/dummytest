6e7de5fa0ea9d6fc7bc14310c31d05472980b4ab
Evan Yang <quantbit@126.com>
2026-05-09 18:47:11 +0800

    Change version tuple

--- _dummytest/_version.py
+++ _dummytest/_version.py
@@ -1,4 +1,4 @@
 # pre version -> beta version -> tomorrow version
-__version__ = "0.0.0-pre10"
-__version_tuple__ = (0, 0, 0, "pre10")
+__version__ = "0.0.0-pre11"
+__version_tuple__ = (0, 0, 0, "pre", 11)
 __commit_id__ = None
