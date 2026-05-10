1cf84d24a5b0f5c76feeee432608446d1a773bdb
Evan Yang <quantbit@126.com>
2026-05-10 11:09:21 +0800

    Dump version

--- _dummytest/_version.py
+++ _dummytest/_version.py
@@ -1,4 +1,4 @@
 # pre version -> beta version -> tomorrow version
-__version__ = "0.0.0-beta2"
-__version_tuple__ = (0, 0, 0, "beta", 2)
+__version__ = "0.0.0-beta3"
+__version_tuple__ = (0, 0, 0, "beta", 3)
 __commit_id__ = None
